cf4b801b3039fca1475b8dd995040a2cbd88454d
preto960 <preto.orellana@gmail.com>
2024-06-20 10:43:33 -0400

    update composer version require

--- src/GauthServiceProvider.php
+++ src/GauthServiceProvider.php
@@ -3,7 +3,6 @@
 namespace Geekpack\gauth;
 
 use Illuminate\Support\ServiceProvider;
-use Geekpack\Api\Models\User;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\Facades\Log;
@@ -12,8 +11,8 @@ class GauthServiceProvider extends ServiceProvider
 {
     public function register()
     {
-        $this->app['router']->aliasMiddleware('valid.api.route', \Geekpack\Api\Http\Middleware\EnsureApiRouteIsValid::class);
-        $this->app['router']->aliasMiddleware('auth:sanctum', \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class);
+        /* $this->app['router']->aliasMiddleware('valid.api.route', \Geekpack\Api\Http\Middleware\EnsureApiRouteIsValid::class);
+        $this->app['router']->aliasMiddleware('auth:sanctum', \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class); */
         $this->app['router']->aliasMiddleware('verified', \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class);
         $this->app['router']->aliasMiddleware('signed', \Illuminate\Routing\Middleware\ValidateSignature::class);
     }
@@ -21,10 +20,10 @@ class GauthServiceProvider extends ServiceProvider
     public function boot()
     {
 
-        $this->publishes([
+        /* $this->publishes([
             __DIR__.'/../config/mail.php' => config_path('mail.php'),
             __DIR__.'/../config/auth.php' => config_path('auth.php'),
-        ], 'config');
+        ], 'config'); */
 
         $this->publishes([
             __DIR__.'/Database/migrations/' => database_path('migrations'),
