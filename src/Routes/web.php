15706ed0657af5c2e4ca6e53a86edf515b773b14
preto960 <preto.orellana@gmail.com>
2024-06-22 21:12:55 -0400

    update composer version require

--- src/Routes/web.php
+++ src/Routes/web.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 use Illuminate\Foundation\Application;
+use Illuminate\Support\Facades\Auth;
 
 Route::get('/', function () {
     return Inertia::render('Welcome', [
@@ -10,6 +11,8 @@ Route::get('/', function () {
         'canRegister' => Route::has('register'),
         'laravelVersion' => Application::VERSION,
         'phpVersion' => PHP_VERSION,
-        'template' => 'Theme1'
+        'template' => 'Theme1',
+        'canAuth' => Auth::user(),
+        'canSystemName' => 'geekosdev'
     ]);
 })->name('home');
