d01b36b701bcc6d3fe6839b1b95ac3f0da52e202
preto960 <preto.orellana@gmail.com>
2024-06-22 21:48:11 -0400

    update composer version require

--- src/GauthServiceProvider.php
+++ src/GauthServiceProvider.php
@@ -32,10 +32,6 @@ class GauthServiceProvider extends ServiceProvider
         $this->app->booted(function () {
             $this->app['router']->pushMiddlewareToGroup('web', HandleInertiaRequests::class);
         });
-
-        if ($this->app->runningInConsole()) {
-            $this->app->make(GauthSeeder::class)->run();
-        }
     }
 
     public function boot()
@@ -85,6 +81,15 @@ class GauthServiceProvider extends ServiceProvider
             \Geekpack\Api\Events\Registered::class,
             \Geekpack\Api\Listeners\SendEmailVerificationNotification::class,
         );
+
+        if ($this->app->runningInConsole()) {
+            $this->seed();
+        }
+    }
+
+    protected function seed()
+    {
+        $this->app->make(GauthSeeder::class)->run();
     }
 }
 
