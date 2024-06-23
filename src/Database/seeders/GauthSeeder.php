a69b11672db640c40d8acdb8f0aee6f2672a0f99
preto960 <preto.orellana@gmail.com>
2024-06-22 22:34:50 -0400

    update composer version require

--- src/Database/seeders/GauthSeeder.php
+++ src/Database/seeders/GauthSeeder.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Geekpack\Gauth\Database\seeders;
+namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
 use Geekpack\Api\Models\ApiRoute;
@@ -13,21 +13,21 @@ class GauthSeeder extends Seeder
             [
                 'type' => 'post', 
                 'route' => 'api/register', 
-                'controller' => 'Geekpack\Api\Http\Controllers\AuthController', 
+                'controller' => 'Geekpack\Gauth\Http\Controllers\AuthController', 
                 'class' => 'register', 
                 'name' => 'api.register'
             ],
             [
                 'type' => 'post', 
                 'route' => 'api/login', 
-                'controller' => 'Geekpack\Api\Http\Controllers\AuthController', 
+                'controller' => 'Geekpack\Gauth\Http\Controllers\AuthController', 
                 'class' => 'login', 
                 'name' => 'api.login'],
             [
 
                 'type' => 'post', 
                 'route' => 'api/logout', 
-                'controller' => 'Geekpack\Api\Http\Controllers\AuthController', 
+                'controller' => 'Geekpack\Gauth\Http\Controllers\AuthController', 
                 'class' => 'logout', 
                 'name' => 'api.logout', 
                 'middleware' => 'auth:sanctum'],
@@ -35,51 +35,51 @@ class GauthSeeder extends Seeder
 
                 'type' => 'post', 
                 'route' => 'api/forgot-password', 
-                'controller' => 'Geekpack\Api\Http\Controllers\AuthController', 
+                'controller' => 'Geekpack\Gauth\Http\Controllers\AuthController', 
                 'class' => 'forgotPassword', 
                 'name' => 'api.forgot-password'],
             [
 
                 'type' => 'post', 
                 'route' => 'api/reset-password', 
-                'controller' => 'Geekpack\Api\Http\Controllers\AuthController', 
+                'controller' => 'Geekpack\Gauth\Http\Controllers\AuthController', 
                 'class' => 'resetPassword', 
                 'name' => 'api.password.update'],
             [
                 'type' => 'get', 
                 'route' => 'api/email/verify', 
-                'controller' => 'Geekpack\Api\Http\Controllers\AuthController', 
+                'controller' => 'Geekpack\Gauth\Http\Controllers\AuthController', 
                 'class' => 'emailVerificationNotice', 
                 'name' => 'verification.notice'],
             [
 
                 'type' => 'get', 
                 'route' => 'api/email/verify/{id}/{hash}', 
-                'controller' => 'Geekpack\Api\Http\Controllers\AuthController', 
+                'controller' => 'Geekpack\Gauth\Http\Controllers\AuthController', 
                 'class' => 'verify', 
                 'name' => 'verification.verify'],
             [
                 'type' => 'post', 
                 'route' => 'api/email/resend', 
-                'controller' => 'Geekpack\Api\Http\Controllers\AuthController', 
+                'controller' => 'Geekpack\Gauth\Http\Controllers\AuthController', 
                 'class' => 'resendVerificationEmail', 
                 'name' => 'verification.send'],
             [
                 'type' => 'get', 
                 'route' => 'login', 
-                'controller' => 'Geekpack\Api\Http\Controllers\AuthController', 
+                'controller' => 'Geekpack\Gauth\Http\Controllers\AuthController', 
                 'class' => 'showLogin', 
                 'name' => 'login'],
             [
                 'type' => 'post', 
                 'route' => 'register', 
-                'controller' => 'Geekpack\Api\Http\Controllers\AuthController', 
+                'controller' => 'Geekpack\Gauth\Http\Controllers\AuthController', 
                 'class' => 'showRegister', 
                 'name' => 'register'],
             [
                 'type' => 'get', 
                 'route' => 'verifyemail', 
-                'controller' => 'Geekpack\Api\Http\Controllers\AuthController', 
+                'controller' => 'Geekpack\Gauth\Http\Controllers\AuthController', 
                 'class' => 'showVerifyEmail', 
                 'name' => 'verifyemail'],
         ];
