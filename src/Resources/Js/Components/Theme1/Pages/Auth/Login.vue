5133376d266c40ae7a61ddd11f642955e645fe19
preto960 <preto.orellana@gmail.com>
2024-06-29 19:28:52 -0400

    update composer version require

--- src/Resources/Js/Components/Theme1/Pages/Auth/Login.vue
+++ src/Resources/Js/Components/Theme1/Pages/Auth/Login.vue
@@ -1,20 +1,22 @@
 <template>
   <Head title="Login" />
   <Toast />
-  <ThemeToggleButton :theme="theme" @toggle-theme="toggleTheme" />
-  <div class="min-h-screen flex items-center justify-center bg-gray-100">
-    <div class="max-w-md w-full bg-white p-6 rounded-lg shadow-md">
-      <h2 class="text-2xl font-semibold text-center text-gray-700">Login</h2>
+  <div class="min-h-screen flex items-center justify-center bg-gray-100 dark:bg-gray-900">
+    <div class="max-w-md w-full bg-white dark:bg-gray-800 p-6 rounded-lg shadow-md">
+      <div class="flex justify-end mb-4">
+        <ThemeToggleButton :theme="theme" @toggle-theme="toggleTheme" />
+      </div>
+      <h2 class="text-2xl font-semibold text-center text-gray-700 dark:text-gray-200">Login</h2>
       <form @submit.prevent="submit">
         <div class="mt-4">
-          <label class="block text-gray-700">Email</label>
-          <input v-model="form.email" type="email" class="w-full mt-2 p-2 border rounded-lg">
+          <label class="block text-gray-700 dark:text-gray-300">Email</label>
+          <input v-model="form.email" type="email" class="w-full mt-2 p-2 border rounded-lg dark:bg-gray-700 dark:text-gray-300">
           <InputError class="mt-2" :message="form.errors.email" />
         </div>
         <div class="mt-4">
-          <label class="block text-gray-700">Password</label>
+          <label class="block text-gray-700 dark:text-gray-300">Password</label>
           <div class="relative">
-            <input v-model="form.password" :type="showPassword ? 'text' : 'password'" class="w-full mt-2 p-2 border rounded-lg">
+            <input v-model="form.password" :type="showPassword ? 'text' : 'password'" class="w-full mt-2 p-2 border rounded-lg dark:bg-gray-700 dark:text-gray-300">
             <button type="button" @click="toggleShowPassword" class="absolute inset-y-0 right-0 px-3 pt-2 flex items-center">
               <i class="pi" :class="showPassword ? 'pi-eye-slash' : 'pi-eye'"></i>
             </button>
@@ -27,9 +29,7 @@
             <span class="ms-2 text-sm text-gray-600 dark:text-gray-400">Remember me</span>
           </div>
           <div>
-            <Link :href="route('forgotpassword')"
-              class="text-sm text-gray-600 hover:text-gray-800 dark:text-gray-400 dark:hover:text-gray-300"
-            >
+            <Link :href="route('forgotpassword')" class="text-sm text-gray-600 hover:text-gray-800 dark:text-gray-400 dark:hover:text-gray-300">
               Forgot Password?
             </Link>
           </div>
@@ -41,10 +41,8 @@
           </button>
         </div>
         <div class="mt-6">New on our platform? 
-          <Link :href="route('register')"
-            class="rounded-md px-3 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white"
-          >
-          Create an account
+          <Link :href="route('register')" class="rounded-md px-3 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white">
+            Create an account
           </Link>
         </div>
       </form>
