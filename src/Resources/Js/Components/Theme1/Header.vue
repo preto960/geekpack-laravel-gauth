faf244061b3517ea310eba00d65a0ed19c0002c2
preto960 <preto.orellana@gmail.com>
2024-06-29 13:29:58 -0400

    update composer version require

--- src/Resources/Js/Components/Theme1/Header.vue
+++ src/Resources/Js/Components/Theme1/Header.vue
@@ -33,14 +33,7 @@
           </Link>
         </div>
       </div>
-      <button @click="toggleTheme" class="text-gray-500">
-        <svg v-if="theme === 'dark'" class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-          <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 3v1m0 16v1m8.66-12.66l-.7.7M4.34 19.66l-.7-.7M21 12h-1M4 12H3m16.66 4.34l-.7.7M6.34 4.34l-.7-.7M17 12a5 5 0 11-10 0 5 5 0 0110 0z"></path>
-        </svg>
-        <svg v-else class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-          <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M20.354 15.354A9 9 0 018.646 3.646a9 9 0 1011.708 11.708z"></path>
-        </svg>
-      </button>
+      <ThemeToggleButton :theme="theme" @toggle-theme="toggleTheme" />
     </div>
   </header>
 </template>
@@ -48,6 +41,7 @@
 <script setup>
 import { ref, defineProps, defineEmits } from 'vue';
 import { Link, router } from '@inertiajs/vue3';
+import ThemeToggleButton from '@/components/ThemeToggleButton.vue';
 
 const props = defineProps({
   isAuthenticated: Boolean,
@@ -59,6 +53,8 @@ const props = defineProps({
 const emit = defineEmits(['toggleAuth', 'toggleTheme']);
 
 const dropdownOpen = ref(false);
+
+const theme = ref('light');
 
 const toggleDropdown = () => {
   dropdownOpen.value = !dropdownOpen.value;
@@ -72,14 +68,9 @@ const logout = () => {
   emit('toggleAuth', false);
 };
 
+
 const toggleTheme = () => {
-  if (props.theme === 'light') {
-    emit('toggleTheme', 'dark');
-    document.documentElement.classList.add('dark');
-  } else {
-    emit('toggleTheme', 'light');
-    document.documentElement.classList.remove('dark');
-  }
+  theme.value = theme.value === 'dark' ? 'light' : 'dark';
 };
 </script>
 
