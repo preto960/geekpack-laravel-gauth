f2e93beab7eb2d5bf1d84bf2648110e4ae96395b
preto960 <preto.orellana@gmail.com>
2024-06-29 13:49:00 -0400

    update composer version require

--- src/Resources/Js/Components/Theme1/Header.vue
+++ src/Resources/Js/Components/Theme1/Header.vue
@@ -71,6 +71,7 @@ const logout = () => {
 
 const toggleTheme = () => {
   theme.value = theme.value === 'dark' ? 'light' : 'dark';
+  document.documentElement.classList.toggle('dark', theme.value === 'dark');
 };
 </script>
 
